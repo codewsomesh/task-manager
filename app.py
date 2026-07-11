26a4ff7a4d0a2fe4d64ae604faf8996ddb0f5081
Somesh Pradhan <someshpradhan51@gmail.com>
2026-07-11 17:09:42 +0530

    fix deployment config

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    socketio.init_app(app, cors_allowed_origins="*", async_mode="eventlet")
+    socketio.init_app(app, cors_allowed_origins="*", async_mode="gevent")
 
     from auth.routes import auth_bp
     from api.tasks import tasks_bp
